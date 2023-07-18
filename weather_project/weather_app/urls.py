d2fb4a52f9aa0cd76935516d9511a1a587e3fd5a
Mantvydas Racickas <mantvydas.racickas@gmail.com>
2023-07-18 08:21:47 +0300

    final commit

--- weather_project/weather_app/urls.py
+++ weather_project/weather_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('weather/', views.weather_data, name='weather_data'),
+    path('cats/', views.cats_data, name='cats_data'),
+    path('catweather/', views.cats_n_weather, name='cats_n_weather'),
 ]
--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,26 +1,50 @@
 from django.shortcuts import render
 from weather_project.local_settings import API_KEY
-
 import urllib.request
 import json
 
 def index(request):
+    return render(request, "main/index.html")
+
+def weather_data(request):
     if request.method == 'POST':
-        city = request.POST['city']
-        source = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY}').read()
-        list_of_data = json.loads(source)
-        data = {
-            "country_code": str(list_of_data['sys']['country']),
-            "coordinate":  str(list_of_data['coord']['lat']) + ', ' + str(list_of_data['coord']['lon']),
-            "temp":  str(list_of_data['main']['temp']) + '°C',
-            "pressure":  str(list_of_data['main']['pressure']),
-            "humidity":  str(list_of_data['main']['humidity']),
-            "main":  str(list_of_data['weather'][0]['main']),
-            "description":  str(list_of_data['weather'][0]['description']),
-            "icon":  list_of_data['weather'][0]['icon'],
-        }
-        print(data)
+        city = request.POST.get('city', '')
+        if city:
+            source = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY}').read()
+            list_of_data = json.loads(source)
+            data = {
+                "country_code": str(list_of_data['sys']['country']),
+                "coordinate":  str(list_of_data['coord']['lat']) + ', ' + str(list_of_data['coord']['lon']),
+                "temp":  str(list_of_data['main']['temp']) + '°C',
+                "pressure":  str(list_of_data['main']['pressure']),
+                "humidity":  str(list_of_data['main']['humidity']),
+                "main":  str(list_of_data['weather'][0]['main']),
+                "description":  str(list_of_data['weather'][0]['description']),
+                "icon":  list_of_data['weather'][0]['icon'],
+            }
+        else:
+            data = {"message": "Please enter a city.."}
     else:
         data = {}
+    return render(request, "main/weather.html", data)
 
-    return render(request, "main/index.html", data)
+def cats_data(request):
+    return render(request, "main/cats.html")
+
+def cats_n_weather(request):
+    if request.method == 'POST':
+        city = request.POST.get('city', '')
+        if city:
+            source = urllib.request.urlopen(f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={API_KEY}').read()
+            list_of_data = json.loads(source)
+            city = city + ': '
+            temperature = str(round(list_of_data['main']['temp'])) + '°C'
+            data = {
+                "city": city, 
+                "temp": temperature
+                }
+        else:
+            data = {"message": "Please enter a city.."}
+    else:
+        data = {}
+    return render(request, "main/cats_n_weather.html", data)
